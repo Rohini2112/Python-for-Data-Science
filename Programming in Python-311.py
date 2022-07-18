081c2852d66823dd69edba3eafccb1fffb642c1a
Rohini Joshi <78068423+Rohini2112@users.noreply.github.com>
2022-07-18 14:56:47 -0500

    Add files via upload

---
+++ Programming in Python-311.py
@@ -0,0 +1,41 @@
+## 2. Programming in Python ##
+
+23+7
+8+42
+
+## 3. The print() Command ##
+
+23+7
+10-6
+12+38
+print(23+7)
+print(10-6)
+print(12+38)
+print(40+4)
+print(200-25)
+print(14+3)
+
+## 4. Python Syntax ##
+
+print(30+10+40)
+print(4)
+print(-3)
+
+## 5. Computer Programs ##
+
+print(34+16)
+print(34)
+-34
+
+## 6. Code Comments ##
+
+# INITIAL CODE
+print(34 + 16)
+print(34)
+print(-34)
+
+## 7. Arithmetical Operations ##
+
+print(16*10)
+print(48/5)
+5**3
